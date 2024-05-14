0e57d99dc99b536726679ff8343fb1d7cd9cb49e
Miz <sef.notirl@gmail.com>
2024-05-14 12:27:28 +0100

    fixed dashboard favicon

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -9,7 +9,7 @@ if(!isset($_SESSION["email"])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="../nav.css">
     <link rel="stylesheet" href="dashboard.css">
     <title>Dashboard</title>
